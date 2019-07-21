4b67900a05d746977180855084a9afb77e076ffe
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-07-21 14:13:43 -0500

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 from database import db_session
 from flask import Flask
 from schema import schema
-
 from flask_graphql import GraphQLView
 
 app = Flask(__name__)
@@ -30,17 +29,14 @@ example_query = """
 }
 """
 
-
 app.add_url_rule(
     "/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
 )
-
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     #db_session.remove()
     print('Aqui no pude')
-
 
 if __name__ == "__main__":
     app.run()
